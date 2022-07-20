de7f40a1b1772de4a68968b7035244b5f07a0ac3
gabin <rlarkqlskb@naver.com>
2022-07-20 17:46:22 +0900

    sssss

---
+++ algorithm/src/backjoon/backjoon_3009.java
@@ -0,0 +1,13 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class backjoon_3009 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+
+}
